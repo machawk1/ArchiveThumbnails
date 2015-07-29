964f5688e8e854dbd3b2ee2f4589e22137f4290d
Mat Kelly <machawk1@gmail.com>
2015-07-29 14:38:58 -0400

    Start of other service integrations.

--- test/index.js
+++ test/index.js
@@ -1,1 +1,12 @@
 require("mocha-jscs")();
+
+var expect = require('chai').expect;
+
+describe('Math', function () {
+  describe('#max', function () {
+    it('returns the biggest number from the arguments', function () {
+      var max = Math.max(1, 2, 10, 3);
+      expect(max).to.equal(10);
+    });
+  });
+});
