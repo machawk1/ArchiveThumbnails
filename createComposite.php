27e3d100df6e7bb6d6f860ddd80699afa0a75453
Mat Kelly <machawk1@gmail.com>
2015-09-24 18:53:32 -0400

    Tweak composite creation for profs.

--- createComposite.php
+++ createComposite.php
@@ -3,47 +3,69 @@ ini_set('display_errors',1);
 ini_set('display_startup_errors',1);
 error_reporting(-1);
 
+
+
 if(!isset($_GET['uri']) || $_GET['uri'] == "") {echo "You must pass a URI parameter"; return;}
 
-$uri = $_GET['uri'];
+$uri = isset($_GET['uri']) ? $_GET['uri'] : '';
+$strategy = isset($_GET['strategy']) ? $_GET['strategy'] : '';
+
 $sanitizedURI = trim(preg_replace('/[\.\/\-]/','',$uri));
 $sanitizedURI = str_replace('www','',$sanitizedURI);
 $sanitizedURI = str_replace('http','',$sanitizedURI);
 
-$files = glob('./screenshots/*'.$sanitizedURI.'_200.png');
-if(count($files) == 0) {
-  echo "Composite not generated for this URI due to lack of files.";
-  return;
-}
+if($strategy != 'all') {
+  generateScreenshot($sanitizedURI, $strategy, true);
+} else {
+  $strategies = array('alSum','interval','temporalInterval','random');
+  for($s=0; $s<count($strategies); $s++) {
+    generateScreenshot($sanitizedURI, $strategies[$s], false);
+  }
 
+  echo "<html><body><h1>All strategies: ".$sanitizedURI."</h1>";
 
-$mod = floor(count($files)/16);
-$str = '';
-$cmd = 'montage ';
-
-$imageCount = 0;
-$selectedImages = array();
-
-// For URIs that have fewer than 16 thumbnails
-if($mod < 1) { $mod = 1;}
-
-
-
-for($f=0; $f<count($files); $f++) {
-  if($f % $mod == 0){
-    array_push($selectedImages,'./'.$files[$f]);
-    ++$imageCount;
-    if($imageCount >= 16) {break;}
+  for($s=0; $s<count($strategies); $s++) {
+    echo '<div style="float: left; border: 3px double red; margin: 5px; text-align: center;"><img style="width: 600px;" src="./composites/'.$strategies[$s].'_'.$sanitizedURI.'_composite.png" /><br />'.$strategies[$s].'</div>';
   }
 }
 
+function generateScreenshot($uri, $strategy, $httpForward=true) {
 
-$im = new Imagick($selectedImages);
-$res = $im->montageImage(new ImagickDraw(), '4x4', '200x150', '0', '0');
-$res->setImageFormat('png');
+  $files = glob('./screenshots/'.$strategy.'_*'.$uri.'_200.png');
+  if(count($files) == 0) {
+    echo "Composite not generated for this URI due to lack of files or bad strategy name. Try alSum, temporalInterval, interval, or random";
+    return;
+  }
 
-$outputFile = './composites/'.$sanitizedURI.'_composite.png';
-$res->writeImage($outputFile);
-header('Location: '.$outputFile);
+
+  $mod = floor(count($files)/16);
+  $str = '';
+  $cmd = 'montage ';
+
+  $imageCount = 0;
+  $selectedImages = array();
+
+  // For URIs that have fewer than 16 thumbnails
+  if($mod < 1) { $mod = 1;}
+
+
+
+  for($f=0; $f<count($files); $f++) {
+    if($f % $mod == 0){
+      array_push($selectedImages,'./'.$files[$f]);
+      ++$imageCount;
+      if($imageCount >= 16) {break;}
+    }
+  }
+
+
+  $im = new Imagick($selectedImages);
+  $res = $im->montageImage(new ImagickDraw(), '4x4', '200x150+1+1', imagick::MONTAGEMODE_UNFRAME, "1x1+2+2");
+  $res->setImageFormat('png');
+
+  $outputFile = './composites/'.$strategy.'_'.$uri.'_composite.png';
+  $res->writeImage($outputFile);
+  if($httpForward) {header('Location: '.$outputFile);}
+}
 
 ?>
--- screenshots/list_nonZero_min15.php
+++ screenshots/list_nonZero_min15.php
@@ -1,4 +1,7 @@
 <?php
+//echo "This script seems to halt the generation script, so it's been disabled.";
+//return;
+
 ini_set('display_errors',1);
 ini_set('display_startup_errors',1);
 error_reporting(-1);
@@ -6,6 +9,7 @@ error_reporting(-1);
 //$src = "list_nonZero_min15.txt";
 $src = "../uris_lulwah_refined.txt";
 $lines = file($src);
+echo "<html><body><head><style type='text/css'>.strategy {width: 100px; float: left; padding-top: 2.5em;}</style></head><body>";
 echo "<p>Run: 128-bit (0,1) SimHash: k=4, AlSummarization, Lulwah's only-IA, n>15 mementos, results trimmed to 4<=x<=16 for each URI-M to match count per strategy for a single URI.</p>";
 echo "<h4>Processed ".count($lines)." URIs from ".$src."</h4><hr /><hr />";
 //print_r($lines);
@@ -13,7 +17,7 @@ foreach($lines as $line) {
   //$uri = explode(",",$line);
   $uri = $line;
   $squishedURI = trim(preg_replace('/[\.\/\-]/','',$uri));
-  $cmd = "ls -1 *".$squishedURI."*_200.png";
+  $cmd = "ls -1 alSum*".$squishedURI."*_200.png";
 
   $filez = preg_split('/\s+/',shell_exec($cmd));
 
@@ -23,7 +27,7 @@ foreach($lines as $line) {
   }
 
   foreach($filez as $fileX) {
-   $bucketId = substr($fileX,-20);
+   $bucketId = substr($fileX,-10);
    if(!array_key_exists($bucketId,$bucketz)) {
     $bucketz[$bucketId] = array();
    }
@@ -38,13 +42,20 @@ foreach($lines as $line) {
     $cURI = preg_replace('/httpweb.*[0-9]+/','',$cURI);
     //$out .= $cURI." (<a href='./cache/simhashes_".$cURI.".json'>cache</a>)<br />";
     $mementoCount = count(array_keys($bucket));
-    $out .= $cURI.' <a href="../createComposite.php?uri='.$cURI.'" target="_blank">(see composite)</a> - '.$mementoCount." memento(s) in AlSummarization. Lulwah reported ".$uri[0]." total mementos.<br />";
+    $out .= '<div style="border-bottom: 2px dotted black; margin-bottom: 7px;">'.$cURI.' (composite: '.
+            '<a href="../createComposite.php?uri='.$cURI.'&strategy=alSum" target="_blank">AlSum</a> - '.
+            '<a href="../createComposite.php?uri='.$cURI.'&strategy=temporalInterval" target="_blank">Temporal Interval</a> - '.
+            '<a href="../createComposite.php?uri='.$cURI.'&strategy=interval" target="_blank">Interval</a> - '.
+            '<a href="../createComposite.php?uri='.$cURI.'&strategy=random" target="_blank">Random</a> - '.
+            '<a href="../createComposite.php?uri='.$cURI.'&strategy=all" target="_blank">all</a>'.
+            ')<br />';
     $cURI = "";
+    $out .= "<p class='strategy'>AlSum:</p>";
     foreach($bucket as $uri) {
       $fullSize = str_replace("_200","",$uri);
-      $out .= '<a href="'.$fullSize.'"><img style="border: 1px solid black; width: 150px;" src="./'.$uri.'" /></a>';
+      $out .= '<img style="border: 1px solid black; width: 150px;" src="./'.$uri.'" />';
     }
-   $out .= "<br/><hr />";
+   $out .= "</div>";
   }
 
   echo $out;
